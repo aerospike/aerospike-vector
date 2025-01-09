0e929269924ef7acdf412978e6bae16a2f4573c3
dylan <dwelch@aerospike.com>
2025-01-09 11:26:22 -0800

    detailed comments

--- prism-image-search/tests/e2e/test_search.py
+++ prism-image-search/tests/e2e/test_search.py
@@ -12,8 +12,10 @@ def test_search(browser, app_url):
     results_div = browser.find_element(By.ID, 'results')
     assert results_div.is_displayed(), "Results div is not displayed"
 
-    # Find all elements within the gallery
+    # Find all elements within the search resultsgallery
     gallery_items = results_div.find_elements(By.CSS_SELECTOR, ".responsive .gallery .search-result")
 
     # Assert there are exactly 1 elements
+    # This assumes that the app was started without adding any additional images to the data file
+    # By default, there is 1 image in the data file
     assert len(gallery_items) == 1, f"Expected 1 elements in the gallery, but found {len(gallery_items)}."
--- quote-semantic-search/tests/e2e/test_search.py
+++ quote-semantic-search/tests/e2e/test_search.py
@@ -12,8 +12,9 @@ def test_search(browser, app_url):
     results_div = browser.find_element(By.ID, 'results')
     assert results_div.is_displayed(), "Results div is not displayed"
 
-    # Find all elements within the gallery
+    # Find all elements within the results gallery
     gallery_items = results_div.find_elements(By.CSS_SELECTOR, ".gallery .responsive .quote")
 
     # Assert there are exactly 5 elements
+    # By default the app returns 5 quotes
     assert len(gallery_items) == 5, f"Expected 5 elements in the gallery, but found {len(gallery_items)}."
